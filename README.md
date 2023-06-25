f54668cf8fe33eb7ebdad612ae100231b17c6343
Nawasan Wisitsingkhon <arikato110011@gmail.com>
2023-06-25 08:49:26 +0700

    change to mongodb

--- README.md
+++ README.md
@@ -1,18 +1,19 @@
 <p align="center">
 <img height="30" width="40" src="https://github.com/tandpfun/skill-icons/raw/main/icons/Prisma.svg" alt="prisma">
 <img height="30" width="40" src="https://github.com/Arikato111/Arikato111/raw/main/icons/typescript-original.svg" alt="typescript">
+<img height="30" width="40" src="https://github.com/Arikato111/Arikato111/raw/main/icons/mongodb-plain.svg" alt="mongodb">
 </p>
 
 # <p align="center">Api Learn Prisma</p>
 
-this is the project to learn and try to use prisma with express.js and mysql database
+this is the project to learn and try to use prisma with express.js and mongodb database
 
 ## Get started
 
 - clone this repository.
 - `pnpm install` or `npm install` to install dependencies.
 - create `.env` like `.env.example`
-- enter your database config at '.env'
+- enter your database config at '.env' (Mongodb)
 - `npx prisma generate` to generate
 - `npx tsc -w` to auto compile TypeScript to JavaScript. 
 - in another terminal run `pnpm dev` or `npm run dev` to run server with **nodemon**
--- src/express.ts
+++ src/express.ts
@@ -1,29 +1,43 @@
 import express from "express";
 import { prisma } from "./main";
+import cors from "cors";
+
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/users", async (req, res) => {
   const users = await prisma.users.findMany();
-  res.status(200).json({ users });
+  res.status(200).json({
+    statusCode: 1,
+    data: users,
+  });
 });
 
 app.get("/add-user", async (req, res) => {
   // simple check emty data of variables
-  if (!req.query.name || !req.query.address || !req.query.username) {
+
+  let u_name = req.body?.name ?? "";
+  let u_address = req.body?.address ?? "";
+  let u_username = req.body?.username ?? "";
+  let u_password = req.body?.password ?? "";
+  if (!u_name || !u_address || !u_username || !u_password) {
     res.status(200).json({
       statusCode: 0,
-      msg: "emty data (name, address, username)",
+      msg: "Emty data (name, address, username, password)",
+      body: req.body,
     });
     return;
   }
   // after checked insert data with prisma
   await prisma.users.create({
     data: {
-      name: req.query.name?.toString() ?? "",
-      address: req.query.address?.toString() ?? "",
-      password: "111",
+      name: u_name,
+      address: u_address,
+      password: u_password,
       phone: "012932030",
-      username: req.query.username?.toString() ?? "",
+      username: u_username,
     },
   });
   // send message to who that sending request
